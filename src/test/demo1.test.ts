8abc006d5370217e015ec4065a38b80f5fa7ce27
Kishor Naik <kishor.naik011.net@gmail.com>
2024-09-06 16:38:28 +0530

    @KishorNaik: Test File Added

---
+++ src/test/demo1.test.ts
@@ -0,0 +1,25 @@
+// All Test Case Run
+//node --trace-deprecation --test --require ts-node/register -r tsconfig-paths/register ./src/test/demo.test.ts
+
+// Debug Mode with test.only Method
+//node --trace-deprecation --test-only --require ts-node/register -r tsconfig-paths/register ./src/test/demo.test.ts
+
+
+import test, { afterEach, describe } from "node:test";
+import expect from "expect";
+import * as sinon from 'sinon';
+
+describe('Demo1 Test', () => {
+
+    afterEach(()=>{
+        sinon.restore();
+    });
+
+    test(`should return false if guard clause failed`, async () => {
+        expect(true).toBe(true);
+    });
+
+    test(`should return false if guard clause throw exception`, async () => {
+        expect(false).toBe(true);
+    });
+});
