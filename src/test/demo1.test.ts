77a696f664338f8ff5b946d955af96cc1126bf18
Kishor Naik <kishor.naik011.net@gmail.com>
2024-09-06 16:38:47 +0530

    @KishorNaik: Demo Test Added

--- src/test/demo1.test.ts
+++ src/test/demo1.test.ts
@@ -4,22 +4,20 @@
 // Debug Mode with test.only Method
 //node --trace-deprecation --test-only --require ts-node/register -r tsconfig-paths/register ./src/test/demo.test.ts
 
-
-import test, { afterEach, describe } from "node:test";
-import expect from "expect";
+import test, { afterEach, describe } from 'node:test';
+import expect from 'expect';
 import * as sinon from 'sinon';
 
 describe('Demo1 Test', () => {
+	afterEach(() => {
+		sinon.restore();
+	});
 
-    afterEach(()=>{
-        sinon.restore();
-    });
+	test(`should return false if guard clause failed`, async () => {
+		expect(true).toBe(true);
+	});
 
-    test(`should return false if guard clause failed`, async () => {
-        expect(true).toBe(true);
-    });
-
-    test(`should return false if guard clause throw exception`, async () => {
-        expect(false).toBe(true);
-    });
+	test(`should return false if guard clause throw exception`, async () => {
+		expect(false).toBe(true);
+	});
 });
